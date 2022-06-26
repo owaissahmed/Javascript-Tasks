7b3d650128bed8d116350d799c77f9515ca12ea2
Owais Ahmed <muhammadowais25122003@gmail.com>
2022-06-26 06:45:05 +0500

    calculator done

---
+++ app.js
@@ -0,0 +1,27 @@
+// CALCULATOR
+
+var num1 = +prompt ("Enter Your First Num")
+var opt = prompt ("Enter Your opt")
+var num2 = +prompt ("Enter Your Last Num")
+
+if (opt === "+") {
+console.log (num1 + num2)
+alert (num1 + num2)
+}
+if (opt === "/") {
+console.log (num1 / num2)
+alert (num1 + num2)
+}
+if (opt === "*") {
+console.log (num1 * num2)
+alert (num1 + num2)
+}
+if (opt === "-") {
+console.log (num1 - num2)
+alert (num1 + num2)
+}
+if (opt === "%") {
+console.log (num1 % num2)
+alert (num1 + num2)
+}
+
