ff632fd9fbf43fea462505a803b6299f3b8798fe
Owais Ahmed <muhammadowais25122003@gmail.com>
2022-06-26 06:46:53 +0500

    head tail done

--- app.js
+++ app.js
@@ -33,3 +33,27 @@ var stars = Math.floor(dic)
 alert ("Guess What Is Coming From Dice")
 alert (stars)
 
+// HEAD TAIL
+
+var choice = prompt(name + " select heads or tails or H/T")
+var choice = choice.toLowerCase()
+var num = Math.random()
+var num2 = Math.round(num)
+
+if (choice === "h" || choice === "heads") {
+    if (num2 === 0) {
+        alert("Its heads..You Win")
+    } else {
+        alert("Its tails..You Lose")
+    }
+}
+else if (choice === "t" || choice === "tails") {
+    if (num2 === 1) {
+        alert("Its tails..You Win")
+    } else {
+        alert("Its heads..You Lose")
+    }
+} else {
+    alert("Invalid value")
+}
+
